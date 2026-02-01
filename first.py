c5625c28ca24243c17ea9efc73e248a64360849b
harry <harry@gmail.com>
2026-02-01 04:55:25 +0000

    updatecode

---
+++ first.py
@@ -0,0 +1,10 @@
+print("Enter first number:")
+num1 = float(input())
+print("Enter second number:")
+num2 = float(input())
+sum = num1 + num2
+print("The sum is:", sum)
+mul = num1 * num2
+print("The product is:", mul)
+sub = num1 - num2
+print("The difference is:", sub)
